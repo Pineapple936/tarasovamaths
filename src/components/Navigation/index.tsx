100efa7a96898f4f93eff5541f85262f075c3d59
pineapple936 <tarasovegor545@gmail.com>
2025-08-08 13:47:35 +0300

    gradient header

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -16,7 +16,7 @@ export default function Navigation() {
 
     return (
         <nav className={style.navigation}>
-            <h1>@tarasovamaths</h1>
+            <h1 className={style.title}>@tarasovamaths</h1>
             <div className={style.navigateBlock}>
                 <button onClick={() => setOpen(prev => !prev)}><i className={`bx ${open ? 'bx-x' : 'bx-menu'}`} /></button>
                 <div className={`${style.navigateLinks} ${open ? style.open : ""}`}>
