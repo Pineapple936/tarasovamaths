7ec4a81b73333d37f39c108f48d3eb1efd1d9111
pineapple936 <tarasovegor545@gmail.com>
2025-09-07 16:33:54 +0300

    code formatting, updating paths for course description photos

--- src/components/ButtonBack/index.tsx
+++ src/components/ButtonBack/index.tsx
@@ -1,19 +1,22 @@
-"use client"
+"use client";
 
 import style from "./index.module.scss";
 
 export default function ButtonBack() {
-    const handleGoBack = () => {
-        if (typeof window !== 'undefined') {
-            window.history.back();
-        }
-    };
+  const handleGoBack = () => {
+    if (typeof window !== "undefined") {
+      window.history.back();
+    }
+  };
 
-    return (
-        <div className={`button-link ${style.wrapper}`}>
-            <button onClick={handleGoBack}>
-                <span><i className='bx bx-arrow-back' />Назад</span>
-            </button>
-        </div>
-    );
+  return (
+    <div className={`button-link ${style.wrapper}`}>
+      <button onClick={handleGoBack}>
+        <span>
+          <i className="bx bx-arrow-back" />
+          Назад
+        </span>
+      </button>
+    </div>
+  );
 }
--- src/components/CourseLayout/index.tsx
+++ src/components/CourseLayout/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ButtonBack from "../ButtonBack";
 
 export default function CourseLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <>
-            {children}
-            <ButtonBack />
-        </>
-    );
+  return (
+    <>
+      {children}
+      <ButtonBack />
+    </>
+  );
 }
