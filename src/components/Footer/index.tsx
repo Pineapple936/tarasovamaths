26f0caa58c408a9c99f7f8951612493c0a687229
pineapple936 <tarasovegor545@gmail.com>
2025-10-28 21:55:02 +0300

    fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,17 +5,17 @@ export default function Footer() {
   return (
     <footer className={style.footer}>
       <div className={style.container}>
-        <div className={style.copyright}>© 2025 Тарасова Екатерина Дмитриевна</div>
-
-        <div className={style.links}>
-          <Link href="https://disk.yandex.ru/i/mk7TFe21ryycVQ">Политика конфиденциальности</Link>
-        </div>
-
         <div className={style.legal}>
           <p>ИП Тарасова Екатерина Дмитриевна</p>
           <p>ИНН 621901706667, ОГРНИП 324620000039262</p>
           <p>Тел: +7 (930) 881-59-30</p>
         </div>
+        <p className={style.copyright}>
+          © 2025 Тарасова Екатерина Дмитриевна |{" "}
+          <Link className={style.links} href="https://disk.yandex.ru/i/mk7TFe21ryycVQ">
+            Политика конфиденциальности
+          </Link>
+        </p>
       </div>
     </footer>
   );
