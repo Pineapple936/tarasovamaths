7572a82a9051a2519471209d88de67b4f12e729b
pineapple936 <tarasovegor545@gmail.com>
2025-12-13 21:48:25 +0300

    try 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,12 +49,12 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const nonce = headers().get("x-nonce") || undefined;
+  const nonce = (await headers()).get("x-nonce") || undefined;
 
   return (
     <html lang="ru">
